57c5a909658d949c7f07d8596bcd945fa3079540
Alexandra Yaschuk <alexandrayaschuk1995@gmail.com>
2025-03-02 17:47:00 +0300

    6 commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import AppMain from "./AppMain.jsx";
+import { AppMain } from "./AppMain.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
